a33e15540083cd85f0e948d9820ca39237104a35
Klaus Kobald <klaus@kobald.com>
2017-08-30 15:08:01 +0200

    import tryout

--- app/controllers/import_controller.rb
+++ app/controllers/import_controller.rb
@@ -1,5 +1,29 @@
 class ImportController < ApplicationController
 
-  def all
+  def index
+
+    file = ENV['IMPORT_CSV_URL']
+    if file.nil?
+      puts "Error: Please set env IMPORT_CSV_URL"
+      next
+    end
+
+    counter = 0
+    ignoredCounter = 0
+    content  = open(file) {|f| f.read }
+    CSV.parse(content, :headers => true) do |row|
+      counter+=1
+      email = row[0].downcase
+      phone = row[1].tr('-', '')
+      unless Ticket.exists?(email: email) and Ticket.exists?(id_code: phone)
+          Ticket.create(:id_code => phone, :email => email)
+      else
+          ignoredCounter+=1
+      end
+    end
+    puts "Found " + counter.to_s + " records"
+    puts "Ignored existing " + ignoredCounter.to_s + " records"
+
+
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -23,7 +23,7 @@ Rails.application.routes.draw do
   get '/pages/:page' => 'pages#show'
   get '/me' => 'users#me'
   get '/howcanihelp' => 'howcanihelp#index'
-  get '/import' => 'import#all'
+  get '/import' => 'import#index'
   
   get '*unmatched_route' => 'application#not_found'
 end
