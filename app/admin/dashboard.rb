007e2771493accc5101921e4254fe9355913fb0d
Klaus Kobald <klaus@kobald.com>
2018-04-10 23:36:05 +0200

    2018

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -3,7 +3,7 @@ ActiveAdmin.register_page "Dashboard" do
   menu priority: 1, label: proc{ I18n.t("active_admin.dashboard") }
 
   content title: proc{ I18n.t("active_admin.dashboard") } do
-    default_coins = 10
+    default_coins = 3
 
     columns do
       column do
